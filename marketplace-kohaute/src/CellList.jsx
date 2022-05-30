5ee2f63876159d21da25c6925e17f00f1a568f5b
Carlos Kohaute <97993704+CarlosKohaute@users.noreply.github.com>
2022-05-30 18:55:42 -0300

    Commit

--- marketplace-kohaute/src/CellList.jsx
+++ marketplace-kohaute/src/CellList.jsx
@@ -4,25 +4,23 @@ function CellList() {
   return (
     <div className="CellList">
       {cells.map((cell, index) => (
-      <div className="CellListItem">
-        <div>
-          <div className="CellListItem__name">{cell.name}</div>
-          <div className="CellListItem__price">{cell.price.toFixed(2)}</div>
-          <div className="CellListItem__description">
-            {cell.description}
+        <div className="CellListItem" key={`CellListItem-${index}`}>
+          <div>
+            <div className="CellListItem__name">{cell.name}</div>
+            <div className="CellListItem__price">{cell.price.toFixed(2)}</div>
+            <div className="CellListItem__description">{cell.description}</div>
+            <div className="CellListItem__actions Actions">
+              <button className="Actions__add Actions__add-fill ">
+                Adicionar
+              </button>
+            </div>
           </div>
-          <div className="CellListItem__actions Actions">
-            <button className="Actions__add Actions__add-fill ">
-              Adicionar
-            </button>
-          </div>
+          <img
+            className="CellListItem__photo"
+            src={cell.photo}
+            alt={`Celular: ${cell.name}`}
+          />
         </div>
-        <img
-          className="CellListItem__photo"
-          src={cell.photo}
-          alt={`Celular: ${cell.name}`}
-        />
-      </div>
       ))}
     </div>
   );
